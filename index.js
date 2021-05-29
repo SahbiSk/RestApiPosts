e12aa7e66595353e1505a338024ce57ad0e950af
Sahbi <sahbici1000@gmail.com>
2021-05-29 14:13:21 +0100

    changed the location of the connection string

--- index.js
+++ index.js
@@ -14,14 +14,16 @@ app.use(cors());
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
-
 app.use("/posts", postRoutes);
 mongoose
-  .connect(DB, {
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  })
+  .connect(
+    "mongodb+srv://user:user@cluster0.6wkb8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }
+  )
   .then(() =>
     app.listen(port, () => console.log("server running on port " + port))
   )
